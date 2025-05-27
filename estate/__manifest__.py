de90dce7f9dd0867a70ebb1073dd3f848183f826
Theodore-000 <teddytez000@gmail.com>
2025-05-27 10:57:55 +0300

    added other description about the module

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -1,3 +1,20 @@
 {
-    "name": "Estate Properties"
+    "name": "Estate Properties", 
+    "author": "Theodore",
+    "version": "1.0.0",
+    "category": "Sales",
+    "summary": "Real Estate Market Place",
+    "description": 
+    """
+    Estate properties managed and marketing on a single platform
+    """,
+    "depends":[],
+    "data": [
+            "security/ir.model.access.csv"
+        ],
+    "sequence": -100,
+    "installable": True,
+    "application": True,
+    "license": "LGPL-3"
+    
 }
