4fb280c3838d61733b834fd0c0f932520c859733
Theodore-000 <teddytez000@gmail.com>
2025-05-27 10:27:29 +0300

    create estate module

---
+++ estate/models/property.py
@@ -0,0 +1,21 @@
+from odoo import fields, models
+
+class Property(models.Model):
+    _name="estate.property"
+    _decription = "Estate Property"
+    
+    name = fields.Char(string="Name", required=True)
+    description = fields.Text(string="Description")
+    postcode = fields.Char(string="Postcode", required=True)
+    date_availability = fields.Date(string="Available From")
+    expected_price = fields.Float(string="Expected Price")
+    selling_price = fields.Float(string="Selling Price")
+    best_offer = fields.Float(string="Best Offer")
+    bedrooms = fields.Integer(string="Bedrooms")
+    living_area = fields.Integer(string="Living Area (sqm)")
+    facades = fields.Integer(string="Facades")
+    garage = fields.Boolean(string="Garage", default=False)
+    garden = fields.Boolean(string="Garden", default=False)
+    garden_area = fields.Integer(string="Garden Area")
+    garden_orientation = fields.Selection([('north', 'North'), ('south', 'South'), ('east', 'East'), ('west', 'West')],string="Garden Orientation")
+    
